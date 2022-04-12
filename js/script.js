4420f5e34a479af72d1a2e8f03c494b91b2e2b05
mustafadede <mustafa.dede.0016@gmail.com>
2022-04-12 15:10:06 +0300

    js and css

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-const button = document.querySelector(".submit")
-console.log(button);
+const ratingForm = document.getElementById("ratingForm")
+const given = document.querySelector(".given")
+const ratingCard = document.querySelector(".ratingCard")
+const resultCard = document.querySelector(".resultCard")
+
+function result(e) {
+    ratingCard.classList.add("notVisible")
+    resultCard.classList.add("Visible")
+    const rateNumber = document.querySelector("input[type='radio']:checked").nextElementSibling.textContent
+    given.innerHTML = `You selected ${rateNumber} out of 5`
+    console.log(rateNumber);
+
+    
+    e.preventDefault()
+
+}
+
+ratingForm.addEventListener("submit", result)
